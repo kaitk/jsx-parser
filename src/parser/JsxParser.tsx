7240df18bd3289db4c861b8d6f3ffc3922ec4239
Kait Kasak <kait@snowhound.eu>
2023-05-18 01:52:02 +0300

    feat: memoize jsx parsing

--- src/parser/JsxParser.tsx
+++ src/parser/JsxParser.tsx
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useMemo, useState } from 'react';
 import Parser, { TProps } from './Parser';
 
 export function JsxParser<T>(props: TProps<T>): React.JSX.Element {
-  const parser = new Parser(props);
-  const parsedChildren = parser.parseJSX(props.jsx!);
+  const [should, setFail] = useState(false);
+  console.warn('THIS WORKS', should);
+
+  const test = () => {
+    console.log(setFail);
+  };
+
+  const parser = new Parser({ ...props, onError: test });
+
+  const parsedChildren = useMemo(
+    () => parser.parseJSX(props.jsx!),
+    [props.jsx]
+  );
   return props.renderInWrapper ? (
     <div className="jsx-parser">{parsedChildren}</div>
   ) : (
