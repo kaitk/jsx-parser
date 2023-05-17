481fed41fe8b103220cd626f7f46f94375f36f21
Kait Kasak <kait@snowhound.eu>
2023-05-18 02:08:17 +0300

    fix: fix for ServerComponents

--- src/parser/JsxParser.tsx
+++ src/parser/JsxParser.tsx
@@ -1,20 +1,14 @@
-import React, { useMemo, useState } from 'react';
+import React, { useMemo } from 'react';
 import Parser, { TProps } from './Parser';
 
 export function JsxParser<T>(props: TProps<T>): React.JSX.Element {
-  const [should, setFail] = useState(false);
-  console.warn('THIS WORKS', should);
-
-  const test = () => {
-    console.log(setFail);
-  };
-
-  const parser = new Parser({ ...props, onError: test });
+  const parser = new Parser(props);
 
   const parsedChildren = useMemo(
     () => parser.parseJSX(props.jsx!),
     [props.jsx]
   );
+
   return props.renderInWrapper ? (
     <div className="jsx-parser">{parsedChildren}</div>
   ) : (
